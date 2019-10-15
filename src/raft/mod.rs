44eea630097a3e232ea5bb37707250fb0904da78
Oron Sharabi <oron@bigpanda.io>
2019-10-15 22:34:10 +0300

    Basic raft node

--- src/raft/mod.rs
+++ src/raft/mod.rs
@@ -1,5 +1,14 @@
 extern crate actix_raft;
-use actix_raft::{Raft};
+
+use actix::prelude::*;
+use actix_raft::{
+    NodeId,
+    Raft,
+    config::{Config, SnapshotPolicy},
+};
+
+use tempfile::{tempdir_in, TempDir};
+use std::time::{Duration};
 
 use crate::network::{Network};
 
@@ -9,3 +18,41 @@ mod storage;
 use self::storage::{MemoryStorageData, MemoryStorageError, MemoryStorage};
 
 pub type MemRaft = Raft<MemoryStorageData, MemoryStorageError, Network, MemoryStorage>;
+
+pub struct RaftNode {
+    id: NodeId,
+    addr: Addr<MemRaft>,
+    members: Vec<NodeId>,
+    network: Addr<Network>,
+}
+
+impl RaftNode {
+    pub fn new(id: NodeId, members: Vec<NodeId>, network: Addr<Network>) -> RaftNode {
+        let id = id;
+        let raft_members = members.clone();
+        let raft_network = network.clone();
+        let metrics_rate = 1;
+        let temp_dir = tempdir_in("/tmp").expect("Tempdir to be created without error.");
+        let snapshot_dir = temp_dir.path().to_string_lossy().to_string();
+        let config = Config::build(snapshot_dir.clone())
+            .election_timeout_min(800).election_timeout_max(1000).heartbeat_interval(300)
+            .metrics_rate(Duration::from_secs(metrics_rate))
+            .snapshot_policy(SnapshotPolicy::Disabled).snapshot_max_chunk_size(10000)
+            .validate().expect("Raft config to be created without error.");
+
+        let storage = MemoryStorage::create(|_| MemoryStorage::new(raft_members, snapshot_dir));
+        let storage_addr = storage.clone();
+
+        let raft_network = network.clone();
+        let addr = Raft::create(move |_| {
+            Raft::new(id, config, raft_network.clone(), storage.clone(), raft_network.recipient())
+        });
+
+        RaftNode {
+            id: id,
+            addr: addr,
+            members: members,
+            network: network,
+        }
+    }
+}
