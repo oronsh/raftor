90dfe655a4d45d324dac98fb2e5afe38bb3dbf3a
Oron Sharabi <oron@bigpanda.io>
2019-10-15 21:40:35 +0300

    Add raft node

--- src/raft/mod.rs
+++ src/raft/mod.rs
@@ -1,4 +1,11 @@
 extern crate actix_raft;
+use actix_raft::{Raft};
+
+use crate::network::{Network};
 
 mod network;
 mod storage;
+
+use self::storage::{MemoryStorageData, MemoryStorageError, MemoryStorage};
+
+pub type MemRaft = Raft<MemoryStorageData, MemoryStorageError, Network, MemoryStorage>;
